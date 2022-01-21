c34458a52bbcc3eb4dacc40a1750086832424042
LucasGabriello <lucasgda59@gmail.com>
2022-01-20 22:30:23 -0300

    add  algoritimos/fibonacci.js

---
+++ algoritimos/fibonacci.js
@@ -0,0 +1,24 @@
+function fibonacci(n){
+    var fibo = [0]
+    if(n==1){return fibo}
+    if(n==2){
+        fibo.push(1) 
+        return fibo
+    }
+    if(n>=3){
+        fibo.push(1) 
+        let x = 0
+        let y = 1
+        let fib = n
+        for(let i = 3; i <=n; i++){
+            fib = y+x
+            x = y
+            y = fib
+            fibo.push(fib)
+        }
+    }
+    
+    return fibo
+}
+
+console.log(fibonacci(6))
